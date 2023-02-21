d871d5875b565bf739d837d725e3f9311fdced01
PrabeshPP <prabesh.bista369@gmail.com>
2023-02-21 18:56:00 -0500

    .

--- course/src/index.js
+++ course/src/index.js
@@ -1,20 +1,5 @@
-const http=require("http");
+const app=require("./server");
 
-const server=http.createServer(async(req,res)=>{
-    if(req.url==="/" && req.method==="GET"){
-        res.writeHead(200,{"Content-Type":"application/json"});
-        res.write(JSON.stringify({message:"hello"}));
-        res.end();
-        return;
-    }else if(req.url==="/prabesh" && req.method==="GET"){
-        res.writeHead(200,{"Content-Type":"application/json"});
-        res.write(JSON.stringify({message:"Hey I am Prabesh"}))
-        res.end();
-    }
-    res.writeHead(404,{"Content-Type":"application/json"});
-    res.end(JSON.stringify({message:"nope"}));
-})
-
-server.listen(3001,()=>{
-    console.log(`Server running at PORT ${3001}`)
-})
+app.listen(3001,()=>{
+    console.log("Server running at Port 3000");
+});
